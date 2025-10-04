fa8a15fabb2796156d51a646454a04dcfb891e11
Narumi Nogawa <narutyan0711@gmail.com>
2025-10-04 22:02:18 +0900

    enable build

--- apps/web/app/dm/page.tsx
+++ apps/web/app/dm/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import DMListContainer from "./_containers/dm-list/container";
 
 export default async function Page() {
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import FeedContainer from "app/_containers/feed/container";
 
 export default async function Page() {
